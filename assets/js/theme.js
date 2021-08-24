d344f926e0572b165c59f2908ec33527e1db066f
public-float-artMajor <ryan.alex.davis@gmail.com>
2021-08-24 12:38:08 -0700

    Light theme set

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -12,16 +12,18 @@ initTheme();
 function initTheme() {
   const savedTheme = localStorage.getItem(STORAGE_KEY);
 
-  if (savedTheme) {
-    // Storage theme
-    setTheme(savedTheme);
-  } else if (window.matchMedia && window.matchMedia(QUERY_KEY).matches) {
-    // system theme
-    setTheme(themes.DARK);
-  } else {
-    // Default theme
-    setTheme(themes.LIGHT);
-  }
+  // if (savedTheme) {
+  //   // Storage theme
+  //   setTheme(savedTheme);
+  // } else if (window.matchMedia && window.matchMedia(QUERY_KEY).matches) {
+  //   // system theme
+  //   setTheme(themes.DARK);
+  // } else {
+  //   // Default theme
+  //   setTheme(themes.LIGHT);
+  // }
+
+  setTheme(themes.LIGHT);
 
   // Watch for system theme changes
   window.matchMedia(QUERY_KEY).addEventListener("change", (e) => {
